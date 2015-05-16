fdaafb9bfad87ead8bd33c9f05ab3ec49436f6c8
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-16 16:32:23 -0700

    add example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQActivityKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQActivityKit
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+}
