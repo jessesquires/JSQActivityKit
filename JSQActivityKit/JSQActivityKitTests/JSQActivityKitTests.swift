9b25e00ea688ce0ed5e7f1669855cb7ef30e169c
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-16 16:18:03 -0700

    add initial framework

---
+++ JSQActivityKit/JSQActivityKitTests/JSQActivityKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQActivityKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQActivityKit
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+import XCTest
+
+
+class JSQActivityKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
