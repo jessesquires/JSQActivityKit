565a84d8e9e0c322feeec5ad9547638bd911eb71
Jesse Squires <jesse.d.squires@gmail.com>
2016-08-03 12:26:06 -0700

    auto-update docs

--- build_docs.sh
+++ build_docs.sh
@@ -6,4 +6,10 @@
 
 git submodule update --remote
 
-jazzy -o ./ --source-directory JSQActivityKit/JSQActivityKit/ --readme JSQActivityKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQActivityKit' -g 'https://github.com/jessesquires/JSQActivityKit'
+jazzy --swift-version 2.2 -o ./ \
+      --source-directory JSQActivityKit/JSQActivityKit/ \
+      --readme JSQActivityKit/README.md \
+      -a 'Jesse Squires' \
+      -u 'https://twitter.com/jesse_squires' \
+      -m 'JSQActivityKit' \
+      -g 'https://github.com/jessesquires/JSQActivityKit'
